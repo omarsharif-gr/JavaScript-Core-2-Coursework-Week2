5c0796a00da6e4b929a20b5633d3cd19e4a0a66c
omarsharifgr <75176327+omarsharif-gr@users.noreply.github>
2021-03-01 23:38:38 +0000

    Revert "Competed all exercises and projects"
    This reverts commit 889b2970e5e00442dd8142a855bb9a83626434df.

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,29 +1,7 @@
 function listOfColours(colours) {
-  let content = document.querySelector("#content");
-  let selectElement = document.createElement("select");
-  content.appendChild(selectElement);
-  let pElement = document.createElement("p");
-  content.appendChild(pElement);
-  for (let colour of colours) {
-    let option1 = document.createElement("option");
-    option1.value = colour;
-    option1.innerText = colour;
-    selectElement.appendChild(option1);
-    selectElement.addEventListener("click", selectColours);
-    function selectColours() {
-      pElement.innerText = `You have selected: ${selectElement.value}`;
-      pElement.style.color = selectElement.value;
-    }
-  }
-  content.appendChild(selectElement);
+  // Write your code here...
 }
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
-listOfColours(colours);
-// ## Exercise
 
-// * Create a `<select>` element.
-// * Create a `<p>` element.
-// * Iterate through the array of colours.
-// * Each element in the array should add a `<option>` to the `<select>`, where the `<option>` is the element in the array.
-// * Each `<option>` should have a 'click' event listener to update the contents and colour of the `<p>` with the selected colour.
-// * All of your HTML should go inside the `<div>` with the id **"content"**.
+const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+
+listOfColours(colours);
