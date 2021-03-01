889b2970e5e00442dd8142a855bb9a83626434df
omarsharifgr <75176327+omarsharif-gr@users.noreply.github>
2021-03-01 23:31:42 +0000

    Competed all exercises and projects
    Completed all exercises and projects assigned.

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,21 @@
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+  let addedContent = document.querySelector("#content")
+  let unorderedList = document.createElement("ul")
+  for (listOfItems of arrayOfPeople) {
+    let listItems = document.createElement("li");
+    listItems.innerText = listOfItems;
+    unorderedList.appendChild(listItems)
+  }
+  addedContent.appendChild(unorderedList)
 }
-
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
-
 shoppingList(shopping);
+
+// Create a list of shopping items on the page.
+
+// ## Exercise
+
+// * You should use an unordered list: `<ul>`.
+// * Each item from the array should be in a `<li>` tag.
+// * All of your HTML should go inside the `<div>` with the id **"content"**.
+
