6ba1ca8a55754a417fa40a9351b1957996fc583e
omarsharifgr <75176327+omarsharif-gr@users.noreply.github>
2021-03-04 09:49:41 +0000

    Revert "Competed all exercises and projects"
    This reverts commit 889b2970e5e00442dd8142a855bb9a83626434df.

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,13 +1,5 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-  for (let individuals of arrayOfPeople) {
-    let h1Tag = document.createElement("h1")
-    h1Tag.innerText = individuals.name;
-    content.appendChild(h1Tag)
-    let h2Tag = document.createElement("h2")
-    h2Tag.innerText = individuals.job;
-    content.appendChild(h2Tag);
-  } 
 }
 
 let people = [
@@ -17,16 +9,3 @@ let people = [
 ];
 
 listOfNames(people);
-
-
-// - Add a `<h1>` tag to the html containing the name of the person.
-// - Add a `<h2>` tag to the html containing the job of the person.
-// - All of your HTML should go inside the `<div>` with the id **"content"**:
-
-// ```html
-// <div id="content">
-//   <h1>{Name Here}</h1>
-//   <h2>{Job Here}</h2>
-//   .....
-// </div>
-// ```
--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,21 +1,7 @@
 function shoppingList(arrayOfPeople) {
-  let addedContent = document.querySelector("#content")
-  let unorderedList = document.createElement("ul")
-  for (listOfItems of arrayOfPeople) {
-    let listItems = document.createElement("li");
-    listItems.innerText = listOfItems;
-    unorderedList.appendChild(listItems)
-  }
-  addedContent.appendChild(unorderedList)
+  // Write your code here...
 }
+
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
+
 shoppingList(shopping);
-
-// Create a list of shopping items on the page.
-
-// ## Exercise
-
-// * You should use an unordered list: `<ul>`.
-// * Each item from the array should be in a `<li>` tag.
-// * All of your HTML should go inside the `<div>` with the id **"content"**.
-
